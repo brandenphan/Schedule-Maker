f1869c2ba6c38729bc0e2666a0f80d88a7fa00fa
Michael Lam <mlam11@uoguelph.ca>
2021-07-05 16:21:53 -0400

    Updated backend module type + added libs + headers

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,14 @@
-const express = require("express");
+import express from 'express'
+import cors from 'cors'
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+// Headers
+app.use(cors());
+app.use(express.json({ limit: '50mb', extended: true }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.get("/api", (req, res) => {
 	res.json({ message: "Hello from Server!" });
